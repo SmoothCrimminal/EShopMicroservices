f8b35a542983821de320018cefe3238c532a0fa5
Marcin Krupiński <krupinskimarcin@outlook.com>
2025-10-05 19:06:21 +0200

    Initial Basket project structure

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+    public record GetBasketResult(ShoppingCart ShoppingCart);
+
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            // TODO       
+
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
+    }
+}
